432d45c38edfcd8c360132c1c7e5fa9ba578d5b1
ahmedc3ca <ahmed.ezzo@epfl.ch>
2021-04-20 14:54:25 +0200

    electron + piste + movement

---
+++ Super Cold/Assets/Scripts/ElectronMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectronMover : MonoBehaviour
+{
+
+    public float electronAcceleration = 200f;
+    public float electronJumpPower = 300f;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = Vector3.forward * Time.deltaTime * electronAcceleration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            rb.velocity += Vector3.up * Time.deltaTime * electronJumpPower;
+        }
+    }
+}
